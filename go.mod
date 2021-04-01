8fcc09cf75e281e5ac723663de0777069a192279
Hasanzoda Muhammadiqbol <kayne99@mail.ru>
2021-04-01 11:46:08 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iqbol007/bank
+
+go 1.16
---
+++ pkg/types/types.go
@@ -0,0 +1,14 @@
+package types
+
+//Money int64
+type Money int64
+
+//Category string
+type Category string
+
+//Payment struct have ID, Amount, Category
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+}
